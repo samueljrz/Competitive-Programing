1aeede9feee0bec583718bea65a5469e4194241c
Samuel Junior <samuelevangelista.ti@gmail.com>
2020-06-11 21:44:27 -0300

    URI

---
+++ URI/1002.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	const double pi = 3.14159;
+	double raio = 0;
+
+	cin >> raio;
+
+	cout << fixed << setprecision(4);
+	cout << "A=" << (pi * (raio * raio)) << endl;
+
+	return 0;
+}
