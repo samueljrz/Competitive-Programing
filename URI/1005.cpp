df4f8445556303e7a1ee312905830480b969a230
Samuel Junior <samuelevangelista.ti@gmail.com>
2020-06-19 19:49:59 -0300

    URI

---
+++ URI/1005.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main() {
+    double a, b;
+    
+    cin >> a >> b;
+    
+    cout << setprecision(5) << fixed;
+    cout << "MEDIA = " << ((a*3.5)+(b*7.5))/11 << endl;
+ 
+    return 0;
+}
