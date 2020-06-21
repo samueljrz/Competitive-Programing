74ba7a1beb442578fd9de307aacc8bad4c606e7e
Samuel Junior <samuelevangelista.ti@gmail.com>
2020-06-21 20:00:36 -0300

    URI

---
+++ URI/1008.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    int n;
+    double h, s;
+    
+    cin >> n >> h >> s;
+    
+    cout << "NUMBER = " << n << endl;
+    cout << setprecision(2) << fixed;
+    cout << "SALARY = U$ " << (h*s) << endl;
+    
+    return 0;
+}
